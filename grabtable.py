9efed7358f9b25b1a705eba52cebd396fe6e9726
mwcolem <mwcolem@gmail.com>
2017-04-15 18:08:02 -0400

    first commit

---
+++ grabtable.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import re
+from urllib.request import urlopen
+
+def get_data(etf):
+	print(etf)
+	url = 'http://chart.finance.yahoo.com/table.csv?s='+ etf +'&a=5&b=15&c=2001&d=3&e=15&f=2017&g=d&ignore=.csv' 
+
+	print(url)
+	data = urlopen(url)
+	
+	with open(etf + '.csv', 'wb') as output:
+	        output.write(data.read())
+
+	output.close()
+
+with open('etfs.txt', 'r') as etfs:
+	for line in etfs:
+		get_data(line.rstrip('\n'))	
+
+etfs.close()
